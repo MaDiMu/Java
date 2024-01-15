0e5e0baef3e7fdfd64a77b5cb91077965a6e276e
Martin Diaz <martinediazmunoz@gmail.com>
2024-01-15 20:12:55 -0300

    Faltria pequeños detalles

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const lista=[];
+
+let nombre = prompt("ingrese su nombre");
+alert("bienvenido " + nombre + " Hagamos la lista!");
+let cantidad = parseInt( prompt ("ingrese cantidad de productos a agregar"));
+for( let i=1; i<= cantidad; i++)
+{lista.push = prompt ("producto")
+
+}
+let termina= confirm(  nombre + " quieres modificar la lista?")
+if (termina) {
+    {let cantidad = parseInt( prompt ("ingrese cantidad de productos a agregar"));
+            for( let i=1; i<= cantidad; i++)
+            {lista.push = prompt ("producto")
+            console.log(lista) }
+             }
+                                    }
+    else(console.log(lista))
+
+
